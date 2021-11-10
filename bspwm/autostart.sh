eb5348912dac9191f8e1a89faca8ba1f45a7341e
Shubham <60225359+theluffysenpaii@users.noreply.github.com>
2021-11-10 13:42:02 +0530

    newfikes

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -7,20 +7,18 @@ function run {
   fi
 }
 
-$HOME/.config/polybar/launch.sh & 
-#exec ./.config/polybar/launch.sh &
-#feh --bg-fill ~/Downloads/w.jpg &
+$HOME/.config/polybar/launch.sh &
 feh --randomize --bg-fill ~/Pictures/wallpapers/*
-#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
 xsetroot -cursor_name left_ptr &
-run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
+run sxhkd -c ~/.config/bspwm/sxhkdrc &
 numlockx on &
-picom --config $HOME/.config/bspwm/picom.conf &
+picom --config $HOME/.config/picom/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-exec flameshot &
 ksuperkey -d &
 dunst &
+#exec /home/luffy/.config/bspwm/scripts/picom-toggle.sh &
+nm-applet &
+greenclip daemon &
+kdeconnect-indicator &
+kdeconnect-settings &
 mpd &
-thunar --daemon &
-exec ~/.config/bspwm/scripts/picom-toggle.sh &
-#nitrogen --restore &
